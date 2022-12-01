3819e335ab9fdafd8e486d98effe8769f291f146
Mirkasim Ali <mirkasim.ali@ibm.com>
2022-12-01 17:40:54 +0530

    Committer: Mirkasim Ali <mirkasim.ali@ibm.com>

--- tf/main.tf
+++ tf/main.tf
@@ -1,7 +1,7 @@
 
 resource "aws_instance" "cluster-instances" {
-    count = 1
-    instance_type = "t2.micro"
+    count = var.desired_cluster_size
+    instance_type = "${var.aws_instance_type}"
     ami = "ami-0b0dcb5067f052a63"
     tags = {
         Name = "terraform-crossplane-${format("instance-%03d", count.index + 1)}"
