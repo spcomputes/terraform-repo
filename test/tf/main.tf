2112bdcbba273977d1fd8a8a63b5688f301d68ad
Sai Pandala1 <sai.pandala1@ibm.com>
2022-12-05 15:17:48 +0530

    changed configuration to include state in backened s3

--- test/tf/main.tf
+++ test/tf/main.tf
@@ -27,8 +27,6 @@ terraform {
         bucket = "tfstatebucketcplanetest"
         key    = "s3-test-remote"
         region = "us-east-1"
-        shared_credentials_file = "aws-creds.ini"
-        workspace_key_prefix = "crossplane-test-remote"
        }
-      }
+}
 
